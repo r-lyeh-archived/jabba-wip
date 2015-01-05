e7f6613ee19bc3cfe740b914b2b2ba7cc56d7d86
r-lyeh <rlyeh.nospam@gmail.com>
2015-01-05 18:54:01 +0100

    tweening;

--- layout.cpp
+++ layout.cpp
@@ -18,12 +18,21 @@ struct rect : public point {
 MEDEA_DEFINE( rect &it, ( (point&)it, it.w, it.h, it.name ) );
 
 struct layout : public rect {
-    std::map< char, rect > children;
+    mutable std::map< char, rect > children;
+
+    const rect &operator[]( char x ) const {
+        return children[ x ];
+    }
+    rect &operator[]( char x ) {
+        return children[ x ];
+    }
 };
 MEDEA_DEFINE( layout &it, ( (rect&)it, it.children ) );
 
 layout rescale( const layout &lyt_, float w, float h ) {
     auto lyt = lyt_;
+    w = w / lyt.w;
+    h = h / lyt.h;
     lyt.x *= w;
     lyt.y *= h;
     lyt.w *= w;
@@ -70,7 +79,7 @@ layout parse( const std::string &text, float resw, float resh, float truetype_as
     // what is the aspect ratio you want to describe?
     float ar = lyt.w / lyt.h * truetype_aspect_ratio;
 
-    lyt = rescale( lyt, resw / lyt.w, resh / lyt.h );
+    lyt = rescale( lyt, resw, resh );
 
     // try to compensate text-editors font ratio (unless you use a 1:1 font)
     for( auto &pair : lyt.children ) {
@@ -90,6 +99,55 @@ std::string debug( const layout &lyt ) {
     return medea::to_json( lyt );
 }
 
+std::string dump( const layout &lyt_, int w = 27, int h = 5 ) {
+    auto lyt = rescale( lyt_, w, h );
+
+    for( auto &pair : lyt.children ) {
+        auto &name = pair.first;
+        auto &rect = pair.second;
+
+        if( rect.w == 0 ) rect.w++;
+        if( rect.h == 0 ) rect.h++;
+    }
+
+    wire::strings screen;
+    for( int y = 0; y < h; ++y ) {
+        screen.push_back( wire::string( w, '.' ) );
+    }
+
+    for( auto &pair : lyt.children ) {
+        auto &name = pair.first;
+        auto &rect = pair.second;
+
+        for( auto y = rect.y; y < rect.y + rect.h; ++y ) 
+        for( auto x = rect.x; x < rect.x + rect.w; ++x ) 
+        screen[ y ][ x ] = name;
+    }    
+
+    return screen.str("\1\n");
+}
+
+#include "deps/sand/sand.hpp"
+#include "deps/sand/sand.cpp"
+#include "deps/tween/tween.hpp"
+#include "deps/rlutil/rlutil.h"
+
+#include <thread>
+std::thread morph( layout &lyt, char from, char to, float secs, int ease ) {
+    return std::thread( [&]{
+        auto &cur = lyt[from], src = cur;
+        auto &dst = lyt[to];
+        sand::chrono lp( secs );
+        while( lp.s() < 1 ) {
+            float dt01 = tween::bouncein( lp.s() ); //tween::ease( lp.s(), ease );
+            cur.x = src.x * (1-dt01) + dst.x * dt01;
+            cur.y = src.y * (1-dt01) + dst.y * dt01;
+            cur.w = src.w * (1-dt01) + dst.w * dt01;
+            cur.h = src.h * (1-dt01) + dst.h * dt01;
+            //sand::sleep(1/60.f);            
+        }
+    });
+}
 
 int main() {
 #   define $QUOTE(...) #__VA_ARGS__
@@ -101,6 +159,26 @@ int main() {
         g.............hhhhhhhhhhhhh
     ); 
 
-    std::cout << debug( parse(test, 1280, 720) ) << std::endl;
+    auto lyt = parse(test, 1280, 720);
+    std::cout << debug( lyt ) << std::endl;
+
+    using namespace rlutil;
+
+    float from = lyt['b'].x;
+    float to = lyt['a'].x;
+
+    hidecursor();
+    cls();
+
+    auto th = morph(lyt,'h','f',3.5,tween::BOUNCEIN);
+
+    sand::chrono lp( 3.5 );
+    while( lp.s() < 1 ) {
+        locate(1, 1);
+        printf("%s", dump(lyt).c_str());
+        fflush(stdout);
+    }
+
+    th.join();
 }
 
